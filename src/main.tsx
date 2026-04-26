6dd3a75b552e2c1ec3bde4db16dcb00bba70e999
cleantimecontacto-star <cleantime.contacto@gmail.com>
2026-04-25 21:08:15 -0400

    Fix PWA: registrar service worker para habilitar instalación

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App";
 import "./index.css";
 import { ErrorBoundary } from "./components/ErrorBoundary";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
